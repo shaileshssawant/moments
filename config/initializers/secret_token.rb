4cc101882a9087b3ff9c0a6401d9f76e5677bfe1
Shailesh Sawant <shailesh.sawant@students.sjsu.edu>
2010-11-27 10:28:11 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moments::Application.config.secret_token = 'd87c98b7fb3a68a1d5564fba133a763ef3b71cf9ecd1fbf64ce12244bbfc424802593d90d5874f3adb285b02166b78495d7fe5200ec289a7d2b99c0cae342259'
